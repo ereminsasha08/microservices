544419cc21ff6ec33c01a04e1c8cbfe5cc7ac83c
ereminsasha08 <SashaEremin08051998@gmail.com>
2023-01-06 13:20:51 +0500

    work time and count from DB

--- ms1/src/main/java/com/testtask/ms1/model/Message.java
+++ ms1/src/main/java/com/testtask/ms1/model/Message.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 
 @Entity
-@Table(name = "message")
+@Table(name = "message", indexes = @Index(columnList = "session_id"))
 @Data
 @NoArgsConstructor
 public class Message {
